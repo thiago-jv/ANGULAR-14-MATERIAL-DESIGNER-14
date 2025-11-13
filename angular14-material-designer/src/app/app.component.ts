ca59acbe08d5147f5efec8f61a856fd12a24c012
Thiago <thiago.henrique.25@hotmail.com>
2025-11-13 13:41:14 -0300

    toolbar

--- angular14-material-designer/src/app/app.component.ts
+++ angular14-material-designer/src/app/app.component.ts
@@ -1,16 +1,35 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver } from '@angular/cdk/layout';
-import { MatSidenav } from '@angular/material/sidenav';
+import { fromEvent, map } from 'rxjs';
+
+export const SCROLL_CONTAINER = 'mat-sidenav-content';
+export const TEXT_LIMIT = 50;
+export const SHADOW_LIMIT = 100;
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular14-material-designer';
 
   public isSmallScreen = false;
+  public popText = false;
+  public applyShadow = false;
+
+
+  ngOnInit(): void {
+    const content = document.getElementsByClassName(SCROLL_CONTAINER)[0]
+    fromEvent(content, 'scroll')
+    .pipe(
+      map(() => content.scrollTop)
+    )
+    .subscribe({
+      next: (value) => this.determineHeader(value)
+    })
+  }
 
   constructor(
     private breakpointObserver: BreakpointObserver){}
@@ -29,5 +48,10 @@ export class AppComponent {
 
     get sidenavMode(){
       return this.isSmallScreen ? 'over' : 'side';
+    }
+
+    determineHeader(top: number){
+      this.popText = top >= TEXT_LIMIT;
+      this.applyShadow = top >= SHADOW_LIMIT;
     }
 }
