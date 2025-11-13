95da951d843aa5126968754d56cb840779a4a760
Thiago <thiago.henrique.25@hotmail.com>
2025-11-13 11:29:34 -0300

    add sidnav - angular material

--- angular14-material-designer/src/app/material/material.module.ts
+++ angular14-material-designer/src/app/material/material.module.ts
@@ -2,19 +2,16 @@ import { NgModule } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 
 @NgModule({
   declarations: [],
-  imports: [
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule
-  ],
   exports: [
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSidenavModule
   ]
 })
 export class MaterialModule { }
