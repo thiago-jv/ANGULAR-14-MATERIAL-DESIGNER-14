362f50d49bc8dc6eb5264bc66d587559d61abd63
Thiago <thiago.henrique.25@hotmail.com>
2025-11-13 11:46:21 -0300

    Sidenav dinâmica - Utilizando RxJS

--- angular14-material-designer/src/app/app.component.ts
+++ angular14-material-designer/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular14-material-designer';
+
+  @ViewChild(MatSidenav) sidenav!: MatSidenav;
+
+  constructor(
+    private breakpointObserver: BreakpointObserver){}
+
+    ngAfterContentInit(): void {
+      this.breakpointObserver.observe(['(max-width: 800px)']).subscribe({
+        next: (res) => {
+          if(res.matches){
+            this.sidenav.mode = 'over'
+            this.sidenav.close();
+          } else {
+            this.sidenav.mode = 'push'
+            this.sidenav.open();
+
+          }
+        }
+      })
+    }
 }
